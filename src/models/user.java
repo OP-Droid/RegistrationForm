429a5ebaff8da10c63886819da1ffcc4cc2b73d6
Opicho <139561775+OP-Droid@users.noreply.github.com>
2025-03-28 20:30:56 +0300

    Update user.java

--- src/models/user.java
+++ src/models/user.java
@@ -1,1 +1,55 @@
+public class User {
+    private String name;
+    private String mobile;
+    private String gender;
+    private String dob;
+    private String address;
 
+    public User(String name, String mobile, String gender, String dob, String address) {
+        this.name = name;
+        this.mobile = mobile;
+        this.gender = gender;
+        this.dob = dob;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
