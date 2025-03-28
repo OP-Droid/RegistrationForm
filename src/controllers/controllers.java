f4168f10f36d26fa5dc978b79103ea4e9a0a5526
Opicho <139561775+OP-Droid@users.noreply.github.com>
2025-03-28 20:26:44 +0300

    Update controllers.java

--- src/controllers/controllers.java
+++ src/controllers/controllers.java
@@ -1,1 +1,25 @@
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserController {
+    public static boolean registerUser(String name, String mobile, String gender, String dob, String address) {
+        String query = "INSERT INTO users (name, mobile, gender, dob, address) VALUES (?, ?, ?, ?, ?)";
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, mobile);
+            preparedStatement.setString(3, gender);
+            preparedStatement.setString(4, dob);
+            preparedStatement.setString(5, address);
+
+            int result = preparedStatement.executeUpdate();
+            return result > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
